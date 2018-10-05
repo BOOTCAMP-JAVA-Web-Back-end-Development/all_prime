c3f4cab481b3d28d9ccdbfdafd9588bca25eba01
thanhdc2018 <hung9dquangtrung>
2018-10-05 09:44:05 +0700

    check prime

---
+++ src/check_prime.java
@@ -0,0 +1,24 @@
+public class check_prime {
+    public static boolean  isprime(int n){
+        boolean res = false;
+        if( n < 2){
+            res = false;
+        }
+        else if ( n == 2 || n == 3){
+            res = true;
+        }
+        else{
+            for ( int i = 4; i < n/2; i ++){
+               if( n % i ==0){
+                   res = false;
+                   break;
+               }
+               else{
+                   res = true;
+               }
+
+            }
+        }
+        return res;
+    }
+}
