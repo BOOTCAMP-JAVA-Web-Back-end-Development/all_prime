0e31f3f2e1c2926758ea3819088882110c7d8722
thanhdc2018 <hung9dquangtrung>
2018-10-05 10:13:54 +0700

    add selection

--- src/prime_under_1000.java
+++ src/prime_under_1000.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
+
 public class prime_under_1000 {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số giới hạn trên của dãy số nguyên tố: ");
+        int n = sc.nextInt();
         int index = 0;
         String s = "";
-        for ( int i = 0; i <= 1000; i ++){
+        System.out.println("Dãy số nguyên tố có giới hạn trên " + n + " là:");
+        for ( int i = 0; i <= n; i ++){
             if (check_prime.isprime(i)){
                 index++;
                 System.out.println("Số nguyên tố thứ " +index + " là: " +i);
